83ea18f17b2e0b9e3a1454c99699ae13a3c4ce9a
davidheeren <davidmheeren@gmail.com>
2026-06-30 18:52:45 -0500

    Leaf HTML Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,6 +14,8 @@ class HTMLNode:
         raise NotImplementedError()
 
     def props_to_html(self):
+        if not self.props:
+            return ""
         result = ""
         for key, value in self.props.items():
             result += f' {key}="{value}"'
@@ -21,3 +23,18 @@ class HTMLNode:
 
     def __repr__(self):
         return f"tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props}"
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag: str | None, value: str, props: dict | None = None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if not self.value:
+            raise ValueError("Must supply a value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+
+    def __repr__(self):
+        return f"tag: {self.tag}, value: {self.value}, props: {self.props}"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHtmlNode(unittest.TestCase):
@@ -15,6 +15,18 @@ class TestHtmlNode(unittest.TestCase):
     def test_repr2(self):
         node = HTMLNode("mytag", "myvalue", [], {"target": "_blank"})
         self.assertEqual(repr(node), "tag: mytag, value: myvalue, children: [], props: {'target': '_blank'}")
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>',)
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
 
 
 if __name__ == "__main__":
