ad9f8666c245a479aa4732ff846ce5ac73aa6ad9
davidheeren <davidmheeren@gmail.com>
2026-06-30 17:55:43 -0500

    test text node

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -5,9 +5,19 @@ from textnode import TextNode, TextType
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
-        node = TextNode("This is a text node", TextType.BOLD)
+        node1 = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
-        self.assertEqual(node, node2)
+        self.assertEqual(node1, node2)
+
+    def test_not_eq(self):
+        node3 = TextNode("Test1", TextType.BOLD)
+        node4 = TextNode("Test2", TextType.BOLD)
+        self.assertNotEqual(node3, node4)
+
+    def test_not_eq_url(self):
+        node5 = TextNode("Text", TextType.BOLD, "test.com")
+        node6 = TextNode("Text", TextType.BOLD)
+        self.assertNotEqual(node5, node6)
 
 
 if __name__ == "__main__":
