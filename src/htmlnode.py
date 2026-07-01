ab467ba2ea28c279ceba8fb916cc79489cb81f84
davidheeren <davidmheeren@gmail.com>
2026-06-30 21:02:08 -0500

    Parent HTML Node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -31,10 +31,26 @@ class LeafNode(HTMLNode):
 
     def to_html(self):
         if not self.value:
-            raise ValueError("Must supply a value")
+            raise ValueError("Leaf node must have a value")
         if not self.tag:
             return self.value
         return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
 
     def __repr__(self):
         return f"tag: {self.tag}, value: {self.value}, props: {self.props}"
+
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str | None, children: list[HTMLNode] | None, props: dict | None = None):
+        super().__init__(tag, None, children, props)
+
+    def to_html(self):
+        if not self.tag:
+            raise ValueError("Parent node must have a tag")
+        if not self.children:
+            raise ValueError("Parent node must have children")
+        result = f"<{self.tag}{self.props_to_html()}>"
+        for c in self.children:
+            result += c.to_html()
+        result += f"</{self.tag}>"
+        return result
